104b2c0758d8fabb970ebf208277d278d5223151
opaboihtlung <opa3134@gmail.com>
2023-11-06 15:27:34 +0530

    monday

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+<MainLayout>
+    <div>Index</div>
+    <Link href="/">Show Page</Link>
+    <div>
+        The message is {{ message }}
+    </div>
+</MainLayout>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue'
+    defineProps({
+        message: String
+    })
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,14 @@
+<template>
+    <MainLayout>
+        <div>Show</div>
+        <Link href="/">Main Page</Link>
+    </MainLayout>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue'
+    defineProps({
+        message: String
+    })
+</script>
