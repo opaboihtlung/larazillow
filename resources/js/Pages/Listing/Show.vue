c5ada66eb28e8f16ab22488f56f48f1318e11c37
opaboihtlung <opa3134@gmail.com>
2024-02-16 15:42:36 +0530

    test

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,13 +1,61 @@
 <template>
-    <div>
-        <ListingAddress :listing="listing" />
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">No images</div>
+        </Box>
+     <div class="md:col-span-5 flex flex-col gap-4">
+        <Box>
+            <template #header>
+                Basic Info
+            </template>
+            <Price :price="listing.price" class="text-2xl font-bold" />
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAddress :listing="listing" class="text-gray-500" />
+        </Box>
+
+        <Box>
+            <template #header>
+                Monthly Payment
+            </template>
+                <div>
+                    <label class="label">Interest rate ({{ interestRate }}%)</label>    
+                    <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1"
+                        class="w-full h-4 bg-gray-200 rounded-lg 
+                        appearance-none cursor-pointer dark:bg0g dark:bg-gray-700" 
+                    />
+                    <label class="label">Duration ({{ duration }})</label>    
+                    <input v-model.number="duration" type="range" min="3" max="35" step="1"
+                        class="w-full h-4 bg-gray-200 rounded-lg 
+                        appearance-none cursor-pointer dark:bg0g dark:bg-gray-700" 
+                    />
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your monthly payment</div>
+                        <Price :price="500" class="text-3xl" />
+                    </div>
+                </div>   
+        </Box>
+     </div>
     </div>
 </template>
 
 <script setup>
-import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingAddress from '@/Components/ListingAddress.vue';
+import ListingSpace from '@/Components/ListingSpace.vue';
+import Price from '@/Components/Price.vue';
+import Box from '@/Components/UI/Box.vue'
+
+import {ref, computed} from 'vue'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
     defineProps({
         listing: Object,
     })
 
+const monthlyPayment = computed(() =>{
+    const principle =props.listing.price
+    const monthlyInterest = interestRate.value / 100 / 12
+    const numberOfpaymentMonths = duration.value * 12  
+})
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,16 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
+import { ZiggyVue } from 'ziggy'
+import { InertiaProgress } from '@inertiajs/progress'
+import '../css/app.css'
+
+InertiaProgress.init({
+  delay: 0,
+  color: '#29d',
+  includeCSS: true,
+  showSpinner: true,
+})
 
 createInertiaApp({
   resolve: name => {
@@ -14,6 +24,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .mount(el)
   },
 })
